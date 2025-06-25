55ff09342f70d7f34eac25826f9619e63b48b88e
ELISAAV9 <eaguil78@gmail.com>
2025-06-25 15:14:07 -0600

    Add files via upload

---
+++ Controllers/TrabajadorController.cs
@@ -0,0 +1,26 @@
+﻿using Final.Datos;
+using Final.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final.Controllers
+{
+    public class TrabajadorController : Controller
+    {
+        [HttpPost]
+        [Route("api/ConsultarTrabajador")]
+        public IActionResult ConsultarTrabajador([FromBody] EntTrabajador consulta)
+        {
+            if (consulta == null)
+            {
+                return NoContent();
+            }
+            else
+            {
+                List<EntTrabajador> entTrabajador = new DatosTrabajador().ConsultarTrabajador(consulta);
+
+                return Ok(entTrabajador);
+            }
+
+        }
+    }
+}
---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,78 @@
+﻿using Final.Datos;
+using Final.Entidades;
+using Final.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Final.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpPost]
+        [Route("api/AgregarUsuario")]
+        public IActionResult AgregarUsuario([FromBody] EntEstudiante agregar)
+        {
+            //Verificar que existen los datos 
+            if (agregar == null)
+            {
+                return NoContent();
+            }
+            else 
+            { 
+                //No regresa nada, nada mas guarda el usuario
+                new DatosEstudiante().GuardarUsuario(agregar);
+                return Ok();
+            }
+      
+        }
+
+        [HttpPost]
+        [Route("api/EliminarUsuario")]
+        public IActionResult EliminarUsuario([FromBody] EntEstudiante eliminar)
+        {
+
+            if (eliminar == null)
+            {
+                return NoContent();
+            }
+            else 
+            {
+                new DatosEstudiante().EliminarUsuario(eliminar);
+                return Ok();
+            }
+
+        }
+        [HttpPost]
+        [Route("api/ModificarUsuario")]
+        public IActionResult ModificarUsuario([FromBody] EntEstudiante modificar)
+        {
+            if (modificar == null)
+            {
+                return NoContent();
+            }
+            else
+            {
+                new DatosEstudiante().ModificarUsuario(modificar);
+                return Ok();
+            }
+            
+        }
+        [HttpPost]
+        [Route("api/ConsultarUsuario")]
+        public IActionResult ConsultarUsuario([FromBody] EntEstudiante consulta)
+        {
+            if (consulta == null)
+            {
+                return NoContent();
+            }
+            else 
+            {
+                EntEstudiante entEstudiante = new DatosEstudiante().ConsultarUsuario(consulta);
+
+                return Ok(entEstudiante);
+            }
+
+        }
+    }
+}

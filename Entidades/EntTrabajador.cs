b1f0e07202a02892739243065b99504bc5b404d2
ELISAAV9 <eaguil78@gmail.com>
2025-06-25 15:13:17 -0600

    Add files via upload

---
+++ Entidades/EntTrabajador.cs
@@ -0,0 +1,17 @@
+﻿namespace Final.Entidades
+{
+    public class EntTrabajador
+    {
+        public int? Id_trabajador { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellidos { get; set; }
+        public int? Edad { get; set; }
+        public string? Sexo { get; set; }
+        public string? Estado_Civil { get; set; }
+        public string? Escolaridad { get; set; }
+        public int? Id_empresa { get; set; }
+        public decimal? PorcentajeValoracion { get; set; }
+        public string? ResultadoValoracion { get; set; }
+
+    }
+}
